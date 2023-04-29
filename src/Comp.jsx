d92ba51d17168e44aa357a8739651f67e0db3604
phatphoom <99587354+phatphoom@users.noreply.github.com>
2023-04-29 18:42:37 +0700

    .slice()

--- src/Comp.jsx
+++ src/Comp.jsx
@@ -9,32 +9,35 @@ const Comp = ({ result }) => {
         meanings.map((meaning) => (
           <div key={meaning.partOfSpeech}>
             <h3>{meaning.partOfSpeech}</h3>
-            <ul>
+            {/* <ul>
               {meaning.definitions.slice(0, 3).map((definition, index) => (
                 <li key={index}>{definition.definition}</li>
               ))}
+            </ul> */}
+            <ul>
+              {meaning.definitions
+                .filter(
+                  (definition) =>
+                    definition.partOfSpeech !== "verb" ||
+                    definition.definition !== "To lament."
+                )
+                .slice(0, 3)
+                .map((definition, index) => (
+                  <li key={index}>{definition.definition}</li>
+                ))}
             </ul>
           </div>
         ))}
       <div>
         <h3>example</h3>
         {meanings &&
-          meanings.map((meaning) => (
-            <div>
-              {meaning.definitions.slice(0, 3).map((example, index) => (
+          meanings.slice(0, 2).map((meaning, index) => (
+            <div key={index}>
+              {meaning.definitions.slice(-3).map((example, index) => (
                 <p key={index}>{example.example}</p>
               ))}
             </div>
           ))}
-      </div>
-      <div>
-        <h3>example</h3>
-        {meanings &&
-          meanings.map((meaning) =>
-            meaning.definitions.map((syn, index) => {
-              <p key={index}>{syn}</p>;
-            })
-          )}
       </div>
     </div>
   );
